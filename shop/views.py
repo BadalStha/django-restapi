96623007f7386dbb067e783638c3453fa6d7f82e
Badal Shrestha <shresthabadal324@gmail.com>
2026-06-03 13:12:35 +0545

    Added prdocutserializer and created product lis API endpoing

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+# this means this endpoint only accepts get requests
+@api_view(['GET'])
+def product_list_api(request):
+    # Fetches all products from database
+    products = Product.objects.all()
+
+    # many = True means we are passing a list of data
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
