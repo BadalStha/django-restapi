878ddf0474ed315c0a2fc2f02a953dfbf05cf422
Badal Shrestha <shresthabadal324@gmail.com>
2026-06-03 14:58:40 +0545

    Added support for creating (POST) and deleting (DELETE) products via API

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/products/', views.product_list_api, name='product_list_api'),
+    path('api/products/<int:pk>/', views.product_detail_api, name='product_detail_api'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,37 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
 # this means this endpoint only accepts get requests
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list_api(request):
-    # Fetches all products from database
-    products = Product.objects.all()
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
-    # many = True means we are passing a list of data
-    serializer = ProductSerializer(products, many=True)
-
-    return Response(serializer.data)
+@api_view(['GET', 'DELETE'])
+def product_detail_api(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response({'message': 'Product deleted successfylly'}, status=status.HTTP_204_NO_CONTENT)
